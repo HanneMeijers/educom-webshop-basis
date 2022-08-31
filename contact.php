8af3451ff6be32fbef4c325b18cd3249b5ecbfe7
HanneMeijers <hanne_meijers@hotmail.com>
2022-08-31 16:29:56 +0200

    Website van Hanne #5

--- contact.php
+++ contact.php
@@ -16,36 +16,50 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if (empty($_POST["salutation"])) {
     $salutationErr = "Aanhef is verplicht";
   } else {
-     $salutation = test_input($_POST["salutation"]);
+     $salutation = cleanupInputFromUser($_POST["salutation"]);
   }
-
+  
+  /* validate the name */
   if (empty($_POST["name"])) {
     $nameErr = "Naam is verplicht";
   } else {
-     $name = test_input($_POST["name"]);
+      // $name = $_POST["name"];
+     $name = cleanupInputFromUser($_POST["name"]);
   }
   
   if (empty($_POST["email"])) {
-    $emailErr = "e-mail is verplicht";
+    $emailErr = "E-mail is verplicht";
   } else {
-     $email = test_input($_POST["email"]);
+     $email = cleanupInputFromUser($_POST["email"]);
   }
     
   if (empty($_POST["phone"])) {
     $phoneErr = "Telefoonnummer verplicht";
   } else {
-     $phone = test_input($_POST["phone"]);
+     $phone = cleanupInputFromUser($_POST["phone"]);
   }    
   
   if (empty($_POST["commPref"])) {
     $commPrefErr = "Communicatievoorkeur verplicht";
   } else {
-     $commPref = test_input($_POST["salutation"]);
+     $commPref = cleanupInputFromUser($_POST["commPref"]);
   }
   
+  if (empty ($_POST ["message"])) {
+    $messageErr = "Bericht is verplicht";
+  } else {
+    $message = cleanupInputFromUser($_POST["message"]); 
 }
 
-function test_input($data) {
+}
+/**
+ * Takes the input of a user and trims the whithespace in front en behind
+ * and removes all html special characters or replaces them with HTML equvalents
+ *
+ * @param string $data the user input
+ * @returns string the cleaned string.
+ */
+function cleanupInputFromUser($data) {
   $data = trim($data);
   $data = stripslashes($data);
   $data = htmlspecialchars($data);
@@ -65,35 +79,41 @@ function test_input($data) {
       <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">;    
     <div>
         <label for="salutation">Aanhef:</label>
-        <select id="salutation" name="salutation" value="<?php echo $salutation; ?>">
+        <select id="salutation" name="salutation">
             <option value="">Maak een keuze</option>
-            <option value="man">Man</option>
-            <option value="woman">Vrouw</option>
-            <option value="other">Anders</option>
+            <option value="man" <?php if ($salutation == "man") { echo "selected"; } ?>>Man</option>
+            <option value="woman" <?php if ($salutation == "woman") { echo "selected"; }?>>Vrouw</option>
+            <option value="other" <?php if ($salutation == "other") { echo "selected"; }?>>Anders</option> 
         </select>
+      <span class="error"> <?php echo $salutationErr; ?></span>
     </div>
     <div>
         <label for="name">Naam:</label>
         <input type="text" id="name" name="name" value="<?php echo $name; ?>" placeholder="Jan Klaassen">
+        <span class="error"> <?php echo $nameErr; ?></span>
     </div>
     <div>
     <label for="e-mail">E-mailadres:</label>
     <input type="email" id="e-mail" name="email" value="<?php echo $email; ?>">
+    <span class="error"> <?php echo $emailErr; ?></span>
     </div>
     <div>
     <label for="phone">Telefoonnummer:</label>
     <input type="tel" id="phone" name="phone" value="<?php echo $phone; ?>">
+    <span class="error"> <?php echo $phoneErr; ?></span>
     </div>
     <div>
-  Communicatievoorkeur:
-    <input type="radio" id="cp_e-mail" name="commPref" value="email"><label for="cp_e-mail">E-mail</label>
-    <input type="radio" id="cp_phone" name="commPref" value="phone"><label for="cp_phone">Telefoon</label>
+    <label for="commPref">Communicatievoorkeur:</label>
+    <input type="radio" id="cp_e-mail" name="commPref" value="email" <?php if ($commPref == "email") { echo "checked"; }?>><label for="cp_e-mail">E-mail</label>
+    <input type="radio" id="cp_phone" name="commPref" value="phone" <?php if ($commPref == "phone") { echo "checked"; }?>><label for="cp_phone">Telefoon</label>
+    <span class="error"> <?php echo $commPrefErr; ?></span>
     </div>
     <div>
     <label for="message">Geef uw bericht:</label>
     </div>
     <div>
     <textarea rows="10" cols="70" id="message" name="message" value="<?php echo $message; ?>"></textarea>
+    <span class="error"> <?php echo $messageErr ?></span>
     </div>
     <div>
   <input type="submit" value="Verzend"> 
